4f7ae5c28eb3a1c7d2950731119a3e2b4b0eb41e
anuroop <anuroopf02@gmail.com>
2023-08-03 23:45:08 +0530

    initial commit

--- lib/homescreen/home_screen.dart
+++ lib/homescreen/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:parkinnapi/homescreen/home_controller.dart';
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
+  HomeScreen({Key? key}) : super(key: key);
+  var controller = Get.put(HomeController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
