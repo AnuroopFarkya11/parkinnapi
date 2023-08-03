22b51d119e910d3c0be349ac2b778e290590e434
daksh <dakshpathak0705@gmail.com>
2023-08-03 23:45:46 +0530

    initial commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("ParkInn APId"),
+        title: const Text("ParkInn APIs"),
       ),
     );
   }
